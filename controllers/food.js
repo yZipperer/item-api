727098cf473ff40c38f0b3f58ceaa4ebe69cc2f5
unknown <yz768737@gmail.com>
2021-05-14 12:07:37 -0600

    Updated allFood Controller
    fixed the allFood controller to also include the new dairy data.

--- controllers/food.js
+++ controllers/food.js
@@ -4,7 +4,7 @@ const dryGoodsData = require('../data/food/dryGoods.json');
 const dairData = require("../data/food/dairy.json");
 
 exports.allFood = (req, res) => {
-    const allFood = [fruitData, cannedData, dryGoodsData];
+    const allFood = [fruitData, cannedData, dryGoodsData, dairyData];
     return res.status(200).json(allFood);
 };
 
