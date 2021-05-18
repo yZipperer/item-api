0106aca1a0914d0a50f85146460c3b345fed8bd0
unknown <yz768737@gmail.com>
2021-05-17 18:04:08 -0600

    Updated docs and routes
    I noticed a disparity with the docs.json file, which was not up to date. So, I fixed it and added all available routes thus far. Also, the default '/' route now returns documentation on the routes. This way, you can easily pull up the routes by just using the api. I also created a new '/all' route. This route will return all of the item data, which was previously a part of the default '/' route.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mainData = require("./data/data.json");
+const docs = require("./docs.json");
 
 const food = require("./routes/food");
 app.use("/", food);
@@ -8,10 +9,12 @@ app.use("/", food);
 app.use(express.static(__dirname + '/images'));
 
 app.get("/", (req, res) => {
-    return res.status(200).json(mainData);
+    return res.status(200).json(docs);
 });
 
-
+app.get("/all", (req, res) => {
+    return res.status(200).json(mainData);
+});
 
 app.listen(8080, function(){
     console.log("Server Started");
