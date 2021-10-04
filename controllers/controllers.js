6870d640588fd0fb2e34e1c62057bd00d3fbabe4
unknown <yz768737@gmail.com>
2021-10-04 11:06:58 -0600

    Major Update
    I have just made a major shift in the focus of this project. Previously, this project only included data from the United States. However, I
    have decided to branch out and include more countries around the world. I have split up the images and data folder. Now, there will be seperate folders for each country. Also, I have modified the main 'food' route. You must enter a country and you will be given the information based on that country.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,63 +1,108 @@
-const data = require('../data/data.json');
-const statsTemplate = require('../data/statsTemplate.json');
+const UnitedStatesData = require('../data/United States/data.json');
+const JapanData = require('../data/Japan/data.json');
+const statsTemplate = require('../data/United States/statsTemplate.json');
 
 exports.allData = (req, res) => {
-    return res.status(200).json(data);
+    allData = {
+        "United States": UnitedStatesData,
+        "Japan": JapanData
+    }
+
+    return res.status(200).json(allData);
 };
 
 exports.food = (req, res) => {
     const category = req.query.cat || "none";
     const brand = req.query.brand || "none";
     const result = [];
-    if(category === "none" && brand != "none") {
-        for (const item in data) {
-            if (data[item].brand === brand){
-                result.push(data[item]);
-            } else if (data[item].brand.includes(brand)){
-                result.push(data[item]);
+    
+    const country = req.params.country;
+
+    if (country === "United States") {
+        if(category === "none" && brand != "none") {
+            for (const item in UnitedStatesData) {
+                if (UnitedStatesData[item].brand === brand){
+                    result.push(UnitedStatesData[item]);
+                } else if (UnitedStatesData[item].brand.includes(brand)){
+                    result.push(UnitedStatesData[item]);
+                }
             }
+            return res.status(200).json(result);
         }
-        return res.status(200).json(result);
-    }
-    else if(brand === "none" && category != "none") {
-        for (const item in data) {
-            if (data[item].category === category){
-                result.push(data[item]);
-            } else if (data[item].category.includes(category)){
-                result.push(data[item]);
+        else if(brand === "none" && category != "none") {
+            for (const item in UnitedStatesData) {
+                if (UnitedStatesData[item].category === category){
+                    result.push(UnitedStatesData[item]);
+                } else if (UnitedStatesData[item].category.includes(category)){
+                    result.push(UnitedStatesData[item]);
+                }
             }
+            return res.status(200).json(result);
         }
-        return res.status(200).json(result);
-    }
-    else if(brand != "none" && category != "none") {
-        for (const item in data) {
-            if (data[item].brand === brand && data[item].category === category){
-                result.push(data[item]);
-            } else if (data[item].brand.includes(brand) && data[item].category.includes(category)){
-                result.push(data[item]);
+        else if(brand != "none" && category != "none") {
+            for (const item in UnitedStatesData) {
+                if (UnitedStatesData[item].brand === brand && UnitedStatesData[item].category === category){
+                    result.push(UnitedStatesData[item]);
+                } else if (UnitedStatesData[item].brand.includes(brand) && UnitedStatesData[item].category.includes(category)){
+                    result.push(UnitedStatesData[item]);
+                }
             }
+            return res.status(200).json(result);
         }
-        return res.status(200).json(result);
-    }
-    else if(category === "none" && brand === "none") {
-        return res.status(200).json(data);
+        else if(category === "none" && brand === "none") {
+            return res.status(200).json(UnitedStatesData);
+        }
+    } else if (country === "Japan") {
+        if(category === "none" && brand != "none") {
+            for (const item in JapanData) {
+                if (JapanData[item].brand === brand){
+                    result.push(JapanData[item]);
+                } else if (JapanData[item].brand.includes(brand)){
+                    result.push(JapanData[item]);
+                }
+            }
+            return res.status(200).json(result);
+        }
+        else if(brand === "none" && category != "none") {
+            for (const item in JapanData) {
+                if (JapanData[item].category === category){
+                    result.push(JapanData[item]);
+                } else if (JapanData[item].category.includes(category)){
+                    result.push(JapanData[item]);
+                }
+            }
+            return res.status(200).json(result);
+        }
+        else if(brand != "none" && category != "none") {
+            for (const item in JapanData) {
+                if (JapanData[item].brand === brand && JapanData[item].category === category){
+                    result.push(JapanData[item]);
+                } else if (JapanData[item].brand.includes(brand) && JapanData[item].category.includes(category)){
+                    result.push(JapanData[item]);
+                }
+            }
+            return res.status(200).json(result);
+        }
+        else if(category === "none" && brand === "none") {
+            return res.status(200).json(JapanData);
+        }
     }
     
 };
 
 exports.stats = (req, res) => {
     const result = statsTemplate;
-    for (const item in data) {
-        if (Array.isArray(data[item].category) === false){
-            const tempCat = data[item].category;
+    for (const item in UnitedStatesData) {
+        if (Array.isArray(UnitedStatesData[item].category) === false){
+            const tempCat = UnitedStatesData[item].category;
             
             if (result.numByCat[tempCat] === 0){
                 result.numOfCats += 1;
             }
             result.numByCat[tempCat] += 1;
-        } else if (Array.isArray(data[item].category) === true){
-            for (const index in data[item].category){
-                const tempCat = data[item].category[index];
+        } else if (Array.isArray(UnitedStatesData[item].category) === true){
+            for (const index in UnitedStatesData[item].category){
+                const tempCat = UnitedStatesData[item].category[index];
                 
                 if (result.numByCat[tempCat] === 0){
                     result.numOfCats += 1;
--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,7 @@ const {
 } = require("../controllers/controllers.js");
 
 router.get("/", allData);
-router.get("/food", food);
-router.get("/stats", stats)
+router.get("/food/:country", food);
+router.get("/stats/", stats)
 
 module.exports = router;
